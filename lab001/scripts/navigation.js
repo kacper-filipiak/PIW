5677d30d05d86f6b8d3a63af8c628b3d2ead3c62
Kacper Filipiak <kisielWrosole@gmail.com>
2024-04-25 16:46:32 +0200

    Reorganised project structure

---
+++ lab001/scripts/navigation.js
@@ -0,0 +1,8 @@
+
+function navigate_to_browse_hotels() {
+  window.location.href = "pages/browse_hotels.html";
+}
+
+function navigate_from_page_to_hotel_page() {
+  window.location.href = "hotel_page.html";
+}
