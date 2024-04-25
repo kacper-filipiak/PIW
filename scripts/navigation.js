3447bd3aa41963d6e8200ad1be1ca43f95ecbb13
Kacper Filipiak <kisielWrosole@gmail.com>
2024-04-25 16:48:24 +0200

    Removed old files

--- scripts/navigation.js
+++
@@ -1,8 +0,0 @@
-
-function navigate_to_browse_hotels() {
-  window.location.href = "pages/browse_hotels.html";
-}
-
-function navigate_from_page_to_hotel_page() {
-  window.location.href = "hotel_page.html";
-}
